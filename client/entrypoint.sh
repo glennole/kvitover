8fc79cd800a44cfd200c987105269c1c9ac828e9
glenn <glenn.ole.haugen@gmail.com>
2020-01-30 00:20:08 +0100

    Updating configs to make subpath work

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,16 +1,28 @@
-#!/bin/bash
-echo "Inserting env variables"
-for file in ./app/js/*.js
+#!/bin/sh
+
+# Replace env vars in JavaScript files
+echo "Replacing env vars in JS"
+for file in /usr/share/nginx/html/js/*.*;
 do
-  echo "env sub for $file"
-  list="$(cat /config_app/env_variables| awk -F= '{print $1}')"
-  echo "$list" | while read -r line; do
-    export REPLACE="REPLACE_$line"
-    export VALUE=$(eval "echo \"\$$line\"")
-    # for debugging use (DO NOT ENABLE IN PRODUCTION):
-    # echo "replacing ${REPLACE} with ${VALUE} in $file"
-    sed -i "s~${REPLACE}~${VALUE}~g" $file
-    unset REPLACE
-    unset VALUE
-  done
+  echo "Processing $file ...";
+
+  # Use the existing JS file as template
+  cp $file $file.tmp
+  envsubst '$VUE_APP_SUB_PATH' < $file.tmp > $file
+  cp $file $file.tmp 
+  envsubst '$VUE_APP_LOGIN_REDIRECT_URL' < $file.tmp > $file
+  cp $file $file.tmp
+  envsubst '$VUE_APP_LOGOUT_REDIRECT_URL' < $file.tmp > $file
+  rm $file.tmp
 done
+
+cp /usr/share/nginx/html/index.html /usr/share/nginx/html/index.html.tmp
+envsubst '$VUE_APP_SUB_PATH' < /usr/share/nginx/html/index.html.tmp > /usr/share/nginx/html/index.html
+rm /usr/share/nginx/html/index.html.tmp
+
+#echo "Replacing env vars in index.html"
+#envsubst '$VUE_APP_BASE_URL' < /usr/share/nginx/html/abaxdashboard/index.html > /usr/share/nginx/html/abaxdashboard/index.html.tmp
+#mv /usr/share/nginx/html/abaxdashboard/index.html.tmp /usr/share/nginx/html/abaxdashboard/index.html
+
+echo "Starting Nginx"
+nginx -g 'daemon off;'
